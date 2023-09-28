3eb2ecc3c8b2f4b215cb0f61ea51e4896f4a23ef
Matheus Hubert <matheushubert@gmail.com>
2023-09-28 21:54:11 +0200

    scrapping

--- junior_dev_scraper/spiders/cars24_com_spider.py
+++ junior_dev_scraper/spiders/cars24_com_spider.py
@@ -1,13 +1,26 @@
 import scrapy
 import json
 import re
-from benedict import benedict
 from w3lib.url import add_or_replace_parameter
 
 # See https://www.cars24.com/ae/buy-used-cars-dubai/
 
 
-class Car24ComSpider(scrapy.Spider):
+class Car24Spider(scrapy.Spider):
     name = 'cars24_com_spider'
+    start_urls = ['https://www.cars24.com/ae/buy-used-cars-dubai/']
 
-    # Good luck
+    def parse(self, response):
+        for car in response.css('div._3IIl_ a._1Lu5u'):
+            yield{
+                'brand': car.css('.RZ4T7::text').get(),
+                'engine_size': car.css('._3ZoHn li~ li+ li::text').get(),
+                'year_of_manufacture' : car.css('._1i1E6::text').get(),
+                'Deeplink': car.css(' a::attr(href) ').get(),
+                'fuel_type': car.css('div.qeOgt::text').get(),
+                'price': car.css('._7yds2::text').get(),
+                'model': car.css('h3.RZ4T7::text').get(),
+                'mileage': car.css('._3ZoHn li:nth-child(2)::text').get()
+            }
+
+            pass
