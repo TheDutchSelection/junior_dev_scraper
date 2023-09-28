cb9fda5d40316e9ac6bc0282f42addbaaf6e1d30
Parthiv Narayan <narayanparthiv@icloud.com>
2023-09-29 00:12:18 +0200

    Assignment

--- junior_dev_scraper/spiders/cars24_com_spider.py
+++ junior_dev_scraper/spiders/cars24_com_spider.py
@@ -1,7 +1,7 @@
 import scrapy
 import json
 import re
-from benedict import benedict
+import benedict
 from w3lib.url import add_or_replace_parameter
 
 # See https://www.cars24.com/ae/buy-used-cars-dubai/
@@ -11,3 +11,22 @@ class Car24ComSpider(scrapy.Spider):
     name = 'cars24_com_spider'
 
     # Good luck
+    start_urls = [
+        "https://www.cars24.com/ae/buy-used-cars-dubai/",
+    ]
+    custom_settings = {
+        'FEEDS': { 'Output.csv': { 'format': 'csv',}}
+    }
+    def parse(self, response):
+        for cars in response.css("div._3IIl_._1xLfH"):
+            year =cars.css("p._1i1E6 ::text").get()[:4]
+            fuel=cars.css("img._3oX3Z ::attr(alt)").get().split(',')[-1][:-4]
+            yield {
+                "Model": cars.css("h3.RZ4T7 ::text").get(),
+                "DeepUrl": cars.css("div._3IIl_._1xLfH a._1Lu5u ::attr(href)").get(),
+                "Price": cars.css("span._7yds2 ::text").get(),
+                "Engine size": cars.css("ul._3ZoHn li::text")[2].get(),
+                "Mileage": cars.css("ul._3ZoHn li::text")[1].get(),
+                "Fuel": fuel,
+                "Year": year,
+            }
